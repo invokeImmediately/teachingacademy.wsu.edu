1f67a54487beae2d5b08b1572423db40c83a490d
invokeImmediately <danielcrieck@gmail.com>
2019-11-14 10:27:25 -0800

    Modify initialization of membership directory filtering
    Rewrite function and associated calling code that initializes the filtering controls for the TA membership directory with several readability enhancements.

--- JS/ta-custom.js
+++ JS/ta-custom.js
@@ -1,45 +1,46 @@
 (function ($) {
-    'use strict';
-    
-    
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // DOM READY
-    
-    $( function() {
-        var $directory;
-        var $filter;
+	'use strict';
+	
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// DOM READY
+	
+	$( function() {
+		initializeDirectoryFiltering( {
+			directorySel: '#tablepress-3_wrapper #tablepress-3',
+			filterSel: '#gform_wrapper_1 #input_1_1',
+			targetCol: 3
+		} );
+	} );
 
-        $directory = $( '#tablepress-3_wrapper #tablepress-3' );
-        $filter = $( '#gform_wrapper_1 #input_1_1' );
-        initializeDirectoryFiltering( $directory, $filter );
-    } );
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // FUNCTIONS
-    
-    function initializeDirectoryFiltering( $directory, $filter ) {
-        var table;
-        var $clearButton;
-        if ($directory && $directory.length && $filter && $filter.length) {
-            console.log( 'Directory & filter found.' );
-            table = $directory.DataTable();
-            $filter.change( function() {
-                var newVal = $filter.val();
-	            console.log( 'Filter value changed; now set to ' + $filter.val() );
-                table.search( '' ).columns().search( '' ).draw();
-                if (newVal != '') {
-                    table.column( 3 ).search( newVal ).draw();
-                }
-            } );
-            $clearButton = $('#clear-filter');
-            if ( $clearButton.length ) {
-                $clearButton.click( function( e ) {
-                    e.preventDefault();
-                    $filter.val('');
-	                table.search( '' ).columns().search( '' ).draw();
-                } );
-            }
-        }
-    }
-    
-})(jQuery);
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// FUNCTIONS
+	
+	function initializeDirectoryFiltering( params ) {
+		var $clearButton = undefined;
+		var $directory = $( params.directorySel );
+		var $filter = $( params.filterSel );
+		var table = undefined;
+ 
+		if ( $directory && $directory.length && $filter && $filter.length ) {
+			console.log( 'Directory & filter found.' );
+			table = $directory.DataTable();
+			$filter.change( function() {
+				var newFiltVal = $filter.val();
+				console.log( 'Filter value changed; now set to ' + $filter.val() );
+				table.search( '' ).columns().search( '' ).draw();
+				if ( newFiltVal != '' ) {
+					table.column( params.targetCol ).search( newFiltVal ).draw();
+				}
+			} );
+			$clearButton = $( '#clear-filter' );
+			if ( $clearButton.length ) {
+				$clearButton.click( function( e ) {
+					e.preventDefault();
+					$filter.val( '' );
+					table.search( '' ).columns().search( '' ).draw();
+				} );
+			}
+		}
+	}
+} )( jQuery );
