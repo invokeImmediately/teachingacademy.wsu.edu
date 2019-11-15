88b27520d39bea6815aa5ce95e173276a35ba2c7
invokeImmediately <danielcrieck@gmail.com>
2019-11-15 14:42:56 -0800

    Add inline documentation to file

--- JS/ta-custom.js
+++ JS/ta-custom.js
@@ -1,9 +1,40 @@
+/*!*************************************************************************************************
+ * https://github.com/invokeImmediately/teachingacademy.wsu.edu/JS/ta-custom.js ↓↓↓
+ * -------------------------------------------------------------------------------------------------
+ * SUMMARY:  Site-specific JS for use on the website of the (WSU President's Teaching Academy)[https
+ *  ://stage.web.wsu.edu/teachingacademy/].
+ *
+ * SUMMARY:  Site-specific JS for the website of the WSU President's Teaching Academy.
+ *
+ * AUTHOR: Daniel Rieck (danielcrieck@gmail.com)[https://github.com/invokeImmediately]
+ *
+ * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ *    without fee is hereby granted, provided that the above copyright notice and this permission
+ *    notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ *    THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ *    SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ *    ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *    CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *    PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS
+// -----------------
+//   §1: DOM Ready Bindings...................................................................39
+//   §2: Membership directory filtering.......................................................50
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ( function ( $ ) {
 	'use strict';
 	
 	
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	// DOM READY
+	//////////////////////////////////////////////
+	// §1: DOM Ready Bindings
 	
 	$( function() {
 		initializeDirectoryFiltering( {
@@ -13,8 +44,8 @@
 		} );
 	} );
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	// FUNCTIONS
+	//////////////////////////////////////////////
+	// §2: Membership directory filtering
 	
 	function initializeDirectoryFiltering( params ) {
 		var $clearButton = undefined;
@@ -42,3 +73,8 @@
 		}
 	}
 } )( jQuery );
+
+/*!
+ * ↑↑↑ ta-custom.js
+ * -------------------------------------------------------------------------------------------------
+ */
